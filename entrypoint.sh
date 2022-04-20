0a752ed82c82235d4db1e03f70bd98388eaf0196
Tommy Trøen <tommy.troen@nav.no>
2022-04-20 14:54:50 +0200

    wip: debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ DOCKER_REGISTRY="${INPUT_IMAGE%%/*}"
 
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
 
-echo "${INPUT_KEY_PWD}" | syft attest --key "${INPUT_KEY}" "${INPUT_IMAGE}" > sbom.json && echo "\n" >> sbom.json
+echo "${INPUT_KEY_PWD}" | syft attest --key "${INPUT_KEY}" "${INPUT_IMAGE}" > sbom.json
 
-cosign attach attestation --attestation sbom.json "${INPUT_IMAGE}"
+echo "printing sbom file:"
+cat sbom.json
+
+#cosign attach attestation --attestation sbom.json "${INPUT_IMAGE}"
