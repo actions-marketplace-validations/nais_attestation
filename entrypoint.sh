2356cdb0c5d8b80bbcc5ba7a411b8f0d04f5c35d
ybelMekk <youssef.bel.mekki@nav.no>
2022-04-20 13:37:14 +0200

    fix: attestation file
    Co-authored-by: Tommy Trøen <tommy.troen@nav.no>
    Co-authored-by: Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>
    Co-authored-by: Jan-Kåre Solbakken <jan-kare.solbakken@nav.no>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ DOCKER_REGISTRY="${INPUT_IMAGE%%/*}"
 
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
 
-echo "${INPUT_KEY_PWD}" | syft attest --output json --key "${INPUT_KEY}" "${INPUT_IMAGE}" >attestation.json
+echo "${INPUT_KEY_PWD}" | syft attest "${INPUT_IMAGE}" -o json=sbom.json --key "${INPUT_KEY}"
 
-cosign attach attestation --attestation attestation.json "${INPUT_IMAGE}"
+cosign attach attestation --attestation sbom.json "${INPUT_IMAGE}"
