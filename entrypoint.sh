a0f38396570960519682a252acf8db7495a57bec
ybelMekk <youssef.bel.mekki@nav.no>
2022-04-20 13:47:31 +0200

    fix: store path
    Co-authored-by: Tommy Trøen <tommy.troen@nav.no>
    Co-authored-by: Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>
    Co-authored-by: Jan-Kåre Solbakken <jan-kare.solbakken@nav.no>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ DOCKER_REGISTRY="${INPUT_IMAGE%%/*}"
 
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
 
-echo "${INPUT_KEY_PWD}" | syft attest --output json --key "${INPUT_KEY}" "${INPUT_IMAGE}" > sbom.json
+echo "${INPUT_KEY_PWD}" | syft attest --output json --key "${INPUT_KEY}" "${INPUT_IMAGE}" > /tmp/sbom.json
 
-cosign attach attestation --attestation sbom.json "${INPUT_IMAGE}"
+cosign attach attestation --attestation /tmp/sbom.json "${INPUT_IMAGE}"
