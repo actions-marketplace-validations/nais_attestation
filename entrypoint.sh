468a0c2c3e0f1a3e3afe674cc8009696c50fc274
ybelMekk <youssef.bel.mekki@nav.no>
2022-04-20 13:40:52 +0200

    fix: move image to last arg
    Co-authored-by: Tommy Trøen <tommy.troen@nav.no>
    Co-authored-by: Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>
    Co-authored-by: Jan-Kåre Solbakken <jan-kare.solbakken@nav.no>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ DOCKER_REGISTRY="${INPUT_IMAGE%%/*}"
 
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
 
-echo "${INPUT_KEY_PWD}" | syft attest "${INPUT_IMAGE}" -o json=sbom.json --key "${INPUT_KEY}"
+echo "${INPUT_KEY_PWD}" | syft attest --output json=sbom.json --key "${INPUT_KEY}" "${INPUT_IMAGE}"
 
 cosign attach attestation --attestation sbom.json "${INPUT_IMAGE}"
