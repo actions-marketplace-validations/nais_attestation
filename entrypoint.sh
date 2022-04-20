a1ac2b2c80d70bc2e8250575cfbe250ee0708dd1
ybelMekk <youssef.bel.mekki@nav.no>
2022-04-20 14:31:16 +0200

    fix: add newline to file
    add CODEOWNERS and gitignore files
    
    Co-authored-by: Tommy Trøen <tommy.troen@nav.no>
    Co-authored-by: Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>
    # Co-authored-by: Jan-Kåre Solbakken <jan-kare.solbakken@nav.no>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ DOCKER_REGISTRY="${INPUT_IMAGE%%/*}"
 
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
 
-echo "${INPUT_KEY_PWD}" | syft attest --key "${INPUT_KEY}" "${INPUT_IMAGE}" > /tmp/sbom.json
+echo "${INPUT_KEY_PWD}" | syft attest --key "${INPUT_KEY}" "${INPUT_IMAGE}" > sbom.json && echo "\n" >> sbom.json
 
-cat /tmp/sbom.json
-
-cosign attach attestation --attestation /tmp/sbom.json "${INPUT_IMAGE}"
+cosign attach attestation --attestation sbom.json "${INPUT_IMAGE}"
