57d6aa94edc59c8a49c0a9ca20c5c203700cd0c1
Tommy Trøen <tommy.troen@nav.no>
2022-04-20 15:07:11 +0200

    wip: debug file creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ DOCKER_REGISTRY="${INPUT_IMAGE%%/*}"
 
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
 
-echo "${INPUT_KEY_PWD}" | syft attest --key "${INPUT_KEY}" "${INPUT_IMAGE}" > sbom.json
+echo "${INPUT_KEY_PWD}" | syft attest --key "${INPUT_KEY}" "${INPUT_IMAGE}" > "$GITHUB_WORKSPACE"/sbom.json
 
 echo "printing sbom file:"
 cat sbom.json
