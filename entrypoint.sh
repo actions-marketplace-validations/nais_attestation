4033e873ff98d2bfce0a57a8f9e1e8bdfa88119a
ybelMekk <youssef.bel.mekki@nav.no>
2022-04-20 13:49:34 +0200

    add: debug
    Co-authored-by: Tommy Trøen <tommy.troen@nav.no>
    Co-authored-by: Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>
    Co-authored-by: Jan-Kåre Solbakken <jan-kare.solbakken@nav.no>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,6 @@ echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USE
 
 echo "${INPUT_KEY_PWD}" | syft attest --output json --key "${INPUT_KEY}" "${INPUT_IMAGE}" > /tmp/sbom.json
 
+cat /tmp/sbom.json
+
 cosign attach attestation --attestation /tmp/sbom.json "${INPUT_IMAGE}"
