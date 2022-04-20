e82ab9ee99aed85b91468239351c2989bf42c5b6
sindrerh2 <sindre.rodseth.hansen@nav.no>
2022-04-20 22:13:09 +0200

    wip: debug

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.14 AS builder
+FROM alpine AS builder
 
 ENV COSIGN_VERSION=v1.6.0
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ echo "docker login:"
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
 
 echo "syft attest:"
+echo "$GITHUB_WORKSPACE"
 
 echo "${INPUT_KEY_PWD}" | syft attest --key "${INPUT_KEY}" "${INPUT_IMAGE}" > "$GITHUB_WORKSPACE"/sbom.json
 
