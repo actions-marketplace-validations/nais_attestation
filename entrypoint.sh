d1b965da64b99b56af14c5452a0f9707c6f3b034
ybelMekk <youssef.bel.mekki@nav.no>
2022-04-20 13:52:16 +0200

    fix: remove --output
    Co-authored-by: Tommy Trøen <tommy.troen@nav.no>
    Co-authored-by: Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>
    # Co-authored-by: Jan-Kåre Solbakken <jan-kare.solbakken@nav.no>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ DOCKER_REGISTRY="${INPUT_IMAGE%%/*}"
 
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
 
-echo "${INPUT_KEY_PWD}" | syft attest --output json --key "${INPUT_KEY}" "${INPUT_IMAGE}" > /tmp/sbom.json
+echo "${INPUT_KEY_PWD}" | syft attest --key "${INPUT_KEY}" "${INPUT_IMAGE}" > /tmp/sbom.json
 
 cat /tmp/sbom.json
 
