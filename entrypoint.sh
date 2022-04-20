54eea57ba1a2684066e20b73864a24b69889a406
sindrerh2 <sindre.rodseth.hansen@nav.no>
2022-04-20 19:57:18 +0200

    wip: debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,13 @@
 
 DOCKER_REGISTRY="${INPUT_IMAGE%%/*}"
 
+docker version
+
+echo "docker login:"
+
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
+
+echo "syft attest:"
 
 echo "${INPUT_KEY_PWD}" | syft attest --key "${INPUT_KEY}" "${INPUT_IMAGE}" > "$GITHUB_WORKSPACE"/sbom.json
 
