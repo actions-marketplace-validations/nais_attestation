9aad5fc9f23ab38c67741e0b5e722d6948abe64e
Tommy Trøen <tommy.troen@nav.no>
2022-04-21 09:37:35 +0200

    doh: --key should be a file

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,7 @@ DOCKER_REGISTRY="${INPUT_IMAGE%%/*}"
 docker version
 
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
+echo "${INPUT_KEY}" > cosign.key
+echo "${INPUT_KEY_PWD}" | syft attest "${INPUT_IMAGE}" > sbom.json
 
-echo "${INPUT_KEY_PWD}" | syft attest --key "${INPUT_KEY}" "${INPUT_IMAGE}"
-
-ls -la
-
-#cosign attach attestation --attestation sbom.json "${INPUT_IMAGE}"
+cosign attach attestation --attestation sbom.json "${INPUT_IMAGE}"
