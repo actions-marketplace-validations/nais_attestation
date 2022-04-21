d81013c6d37c5b53d10bb465792e2a047497c5cd
Tommy Trøen <tommy.troen@nav.no>
2022-04-21 10:17:23 +0200

    feat: use spdx-json format for sbom

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ docker version
 
 echo "$INPUT_DOCKER_PWD" | docker login "$DOCKER_REGISTRY" -u "$INPUT_DOCKER_USER" --password-stdin
 echo "${INPUT_KEY}" > cosign.key
-echo "${INPUT_KEY_PWD}" | syft attest "${INPUT_IMAGE}" > sbom.json
+echo "${INPUT_KEY_PWD}" | syft attest -o spdx-json "${INPUT_IMAGE}" > sbom.json
 
 cosign attach attestation --attestation sbom.json "${INPUT_IMAGE}"
